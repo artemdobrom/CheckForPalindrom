9bf0da9e8a16450637b47571f3eedbe65af2911e
artjom <artemdobrom@gmail.com>
2023-08-08 19:25:42 +0300

    FirstCourseWorkIsDone

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
 
-public class Main {
-    public static void main(String[] args) {
+        public class Main {
+            public static void main(String[] args) {
+                System.out.println("Проверка на полиндром");
+                char[] c = {'s', 'u', 'u', 's'};
+                int first = c[0];
+                int second = c[1];
+                int three = c[2];
+                int four = c[3];
 
-    }
-}
+
+                System.out.println();
+                if (first==four && second==three ) {
+                    System.out.println("это палиндром");
+                } else {
+                    System.out.println("это не палиндром");
+                }
+            }
+        }
+
